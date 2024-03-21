1dbd463a23b917c82bf5765b1cb4ae708d19c352
friend-0228 <kwj2712@gamil.com>
2024-03-22 00:33:15 +0900

    TEST : login 추가

--- src/users/login/repository/loginRepository.js
+++ src/users/login/repository/loginRepository.js
@@ -1,4 +1,4 @@
-import { prisma } from '../../../utils/prisma/index.js'; // prisma 인스턴스 임포트
+import { prisma } from '../../../utils/prisma/index.js'; // prisma 인스턴스 임포트 //
 
 // 이메일로 사용자 찾기
 export const findUserByEmail = async (email) => {
