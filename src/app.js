85e087ce520588ad6ebe33be89eb7e6b53172cf6
friend-0228 <kwj2712@gamil.com>
2024-03-21 20:35:51 +0900

    REFACTOR : 회원가입 시 닉네임 자동추가

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import PostRouter from './posts/router/posts.router.js';
-import SignUpRouter from './users/signup/router/signup.router.js';
+import SignUpRouter from './users/signUp/router/signUp.router.js';
 import LogInRouter from './users/login/router/login.router.js';
 import RefreshTokenRouter from './users/login/router/refreshToken.router.js';
+import CommentRouter from './comments/router/comments.router.js';
 import LikeRouter from './likes/router/likes.router.js';
+import ImageRouter from './images/router/images.router.js';
 
 const app = express();
 const PORT = 3000;
@@ -23,7 +25,7 @@ app.use(
 );
 
 // 라우터 설정
-app.use('/', [PostRouter, SignUpRouter, LogInRouter, RefreshTokenRouter, LikeRouter]);
+app.use('/', [PostRouter, SignUpRouter, LogInRouter, RefreshTokenRouter, CommentRouter, LikeRouter, ImageRouter]);
 
 app.listen(PORT, () => {
     console.log(`${PORT} 포트로 서버가 열렸어요!`);
