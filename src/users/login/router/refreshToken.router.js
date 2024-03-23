14f4693c5b96716a0fb97b63f208e916ccf2db81
friend-0228 <kwj2712@gamil.com>
2024-03-24 05:12:14 +0900

    REFACTOR : 회원가입시 nodemailer로 메일 발송, 회원가입/로그인 합치기

--- src/users/login/router/refreshToken.router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { refreshTokenController } from '../controller/refreshTokenController.js';
-
-const router = express.Router();
-
-// 리프레시 토큰 발급
-router.post('/refresh', refreshTokenController);
-
-export default router;
