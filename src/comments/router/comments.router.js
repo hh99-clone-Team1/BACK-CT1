2ae45b853bcbf8da1c91aedd8afeee99d2498844
friend-0228 <kwj2712@gamil.com>
2024-03-25 11:39:09 +0900

    CONFLICT : 병합 시 충돌

--- src/comments/router/comments.router.js
+++ src/comments/router/comments.router.js
@@ -10,6 +10,51 @@ import {
 
 let router = express.Router();
 
+/**
+ * @swagger
+ * /comments:
+ *   post:
+ *     summary: 댓글 작성
+ *     description: 새로운 댓글을 작성합니다.
+ *     tags: [Comments]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - postId
+ *               - content
+ *             properties:
+ *               postId:
+ *                 type: string
+ *                 description: 포스트 ID
+ *               content:
+ *                 type: string
+ *                 description: 댓글 내용
+ *     responses:
+ *       201:
+ *         description: 댓글이 성공적으로 작성되었습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 댓글이 성공적으로 작성되었습니다.
+ *       400:
+ *         description: 데이터 형식이 올바르지 않습니다.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 데이터 형식이 올바르지 않습니다.
+ */
 // 댓글 작성
 router.post('/posts/:postId/comments', authMiddleware, createCommentController);
 
